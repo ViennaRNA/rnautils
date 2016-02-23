60861ab961ebb506f45f5a5d0af4b3aebdc5b872
Peter Kerpedjiev <pkerp@tbi.univie.ac.at>
2016-02-23 13:51:20 +0100

    Added ColorScheme and updated README

--- test/rnautils_test.js
+++ test/rnautils_test.js
@@ -1,4 +1,5 @@
 import {rnaUtilities} from '../app/scripts/rnautils.js'
+import {ColorScheme} from '../app/scripts/rnautils.js'
 
 describe('RNA utilities unit tests', function() {
     let pt = rnaUtilities.dotbracketToPairtable('.((..)).');
@@ -12,5 +13,17 @@ describe('RNA utilities unit tests', function() {
         let elements = rnaUtilities.ptToElements(pt, 0, 1, pt[0]);
 
         expect(elements.length).toEqual(3);
+    });
+});
+
+describe('RNA color scheme tests', function() {
+    it('should parse ordinal colors', function() {
+        let cs = new ColorScheme('domain=0:10 range=red:green 1: 0.7 2:0.8');
+
+        /*
+        console.log('cs:', cs);
+        console.log('cs.colorJson.colorValues:', cs.colorsJson.colorValues);
+        */
+
     });
 });
